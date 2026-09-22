f2dab8708ec42cc9f4bafd30dfa1b33d5d3583b8
Muhammad Daffa Akbar <dakbar273@gmail.com>
2026-09-22 22:07:15 +0700

    pertemuan 3 routing,controller,dan blade dasar

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title','Kontak Kami')
+
+@section('content')
+<h1>Kontak Kami</h1>
+<p?>Silahkan Hubungi Kami melalui  email di perpus@gmail.com untuk informasi lebih lanjut </p>
+
+@endsection
---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title','Tentang Aplikasi')
+
+@section('content')
+<h1>Tentang Aplikasi</h1>
+<p>Aplikasi ini dibuat sebagai studi kasus pembelajaran mata kuliah pwmograman web 2, berupa sistem informasi peminjaman buku perpustakaan mini</p>
+
+@endsection
